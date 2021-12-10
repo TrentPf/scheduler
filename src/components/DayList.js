9b01e0c12888d45881adc2e2060cd5d5f425b6a8
Trent Pfeiffer <trentpf@hotmail.com>
2021-12-10 16:04:06 -0500

    Add ability to retrieve interviewers for each day

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,7 +4,7 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
   const dayItems = props.days.map((day) =>  
-    <DayListItem key={day.id} name={day.name} spots={day.spots} selected={day.name === props.value} setDay={props.onChange} />
+    <DayListItem key={day.id} name={day.name} spots={day.spots} selected={day.name === props.value} setDay={props.setDay} />
   );
   return (
     <ul>
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -18,7 +18,7 @@ export default function DayListItem(props) {
 
   const dayClass = classNames("day-list__item", {"day-list__item--selected": props.selected}, {"day-list__item--full": !props.spots})
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)} remaining</h3>
     </li>
